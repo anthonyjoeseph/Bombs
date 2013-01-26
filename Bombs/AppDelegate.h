dc319a852c6a275bf52c180598d7bea24236cf73
AnthonyGabriele <anthonygabriele@apples-MacBook-Pro.local>
2013-01-26 16:54:48 -0500

    Initial Commit

---
+++ Bombs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bombs
+//
+//  Created by AnthonyGabriele on 1/26/13.
+//  Copyright (c) 2013 AnthonyGabriele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bombs/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Bombs
+//
+//  Created by AnthonyGabriele on 1/26/13.
+//  Copyright (c) 2013 AnthonyGabriele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
